24638da92428718fa381f2c9c18291f0a56c7ec3
Christian <s194255@student.dtu.dk>
2022-11-16 16:08:01 +0100

    Update sentiment.py

--- misc/sentiment.py
+++ misc/sentiment.py
@@ -5,15 +5,26 @@ import pandas as pd
 import collections
 from nltk.tokenize import RegexpTokenizer
 from tqdm import tqdm
+import os
+import datetime
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+
 
 class Sentimentor:
     def __init__(self, twitter_data_path, sentiment_path):
-        self.tweet_df = pd.read_csv(twitter_data_path)
-        self.tweet_df = self.tweet_df.iloc[0:30]
+        self.tweet_df = pd.read_csv(twitter_data_path).dropna()
+
+        idxs = random.sample(list(np.arange(len(self.tweet_df))), min(1*10**5, len(self.tweet_df)))
+        self.tweet_df = self.tweet_df.iloc[idxs]
 
         self.sentiment_df = pd.read_csv(sentiment_path, sep="\t")
 
-    def fun(self, tweet):
+        self.run()
+
+    def get_sentiment(self, tweet):
         tokenizer = RegexpTokenizer(r'\w+')
         tokens = tokenizer.tokenize(tweet)
 
@@ -35,13 +46,71 @@ class Sentimentor:
             score = 0.0
         return score
 
-    def get_sentiments(self):
+    def get_week_number(self, tweet_timestamp):
+        day = tweet_timestamp.split(" ")[0].split("-")
+        date_obj = datetime.date(int(day[0]), int(day[1]), int(day[2]))
+        week_number = date_obj.isocalendar()[1]
+        return week_number
+
+
+    def run(self):
         tqdm.pandas()
-        sentiments =  self.tweet_df['tweet'].progress_apply(self.fun)
+        sentiments =  self.tweet_df['tweet'].progress_apply(self.get_sentiment)
+
+        tqdm.pandas()
+        week_numbers = self.tweet_df['tweet_timestamp'].progress_apply(self.get_week_number)
 
         self.tweet_df['sentiment_score'] = sentiments
+        self.tweet_df['week_number'] = week_numbers
         return self.tweet_df
 
+    def group(self):
+        grouped_by = {"mean" : self.tweet_df.groupby("week_number")['sentiment_score'].mean(),
+                      'std': self.tweet_df.groupby("week_number")['sentiment_score'].std(),
+                      'N': self.tweet_df.groupby("week_number")['sentiment_score'].count()}
+        return grouped_by
 
-tweet_with_sentiment_df = Sentimentor("cleaned_twitter_data.csv", "Data_Set_S1.txt").get_sentiments()
-tweet_with_sentiment_df.to_csv("twitter_data_with_sentiment.csv", index=False)
+
+a = 2
+sentimentor = Sentimentor("../../cleaned_twitter_data.csv", "../../Data_Set_S1.txt")
+tweet_with_sentiment_df = sentimentor.tweet_df
+tweet_with_sentiment_df.to_csv("../../twitter_data_with_sentiment.csv", index=False)
+
+
+grouped_by = sentimentor.group()
+week_numbers = grouped_by['mean'].keys()._data
+means = grouped_by['mean'].values
+stds = grouped_by['std'].values
+Ns = grouped_by['N'].values
+
+
+plt.plot(week_numbers, means, linewidth=9)
+
+# lower confidence
+lower = [stats.norm.interval(0.95, loc=means[i], scale=stds[i]/np.sqrt(Ns[i]))[0] for i in range(len(Ns))]
+plt.plot(week_numbers, lower, color='black')
+
+
+# upper confidence
+higher = [stats.norm.interval(0.95, loc=means[i], scale=stds[i]/np.sqrt(Ns[i]))[1] for i in range(len(Ns))]
+plt.plot(week_numbers, higher, color='black')
+
+for month_label, month_number in [('April', 4), ('May', 5), ('June', 6) , ('July', 7)]:
+    vline = datetime.date(2020, month_number, 1).isocalendar()[1]
+    plt.vlines(vline, min(lower), max(higher), colors='black', label=month_label, linewidth=4, linestyles='dashed')
+    plt.text(vline+0.1, min(lower), month_label, fontsize=20)
+# plt.scatter(grouped_by.keys()._data, grouped_by.values, s=[300]*len(grouped_by), color='red')
+
+
+
+plt.fill_between(week_numbers, lower, higher, alpha=0.2)
+
+
+
+plt.xticks(week_numbers)
+plt.title("Sentiment score through the weeks", fontsize=20)
+plt.xlabel("Week number", fontsize=20)
+plt.ylabel("Sentiment score", fontsize=20)
+# plt.grid()
+plt.show()
+
